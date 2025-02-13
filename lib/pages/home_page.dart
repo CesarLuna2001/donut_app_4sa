6af842ffdd60825bf920c0128af1bb70f750ec43
CesarLuna2001 <cesarluna200115@outlook.com>
2025-02-13 13:35:05 -0600

    Main Text Added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -16,8 +16,41 @@ class _HomePageState extends State<HomePage> {
         //Icono de la izquierda
         leading: Icon(Icons.menu,
          color: Colors.grey[800],),
-         actions: [Icon(Icons.person)],
-      )
-    );
+         actions: [Padding(
+           padding: const EdgeInsets.only(right: 24.0),
+           child: Icon(Icons.person),
+         )],
+      ),
+      body: Column(
+        children:[
+        //1, Texto principal (Text)
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+          child: Row(
+            children: [
+              Text("I Want to "),
+              Text("Eat", style: TextStyle(
+                //Tamaño de letras
+                fontSize: 32,
+                //Negritas
+                fontWeight: FontWeight.bold,
+                //Subrayado 
+                decoration: TextDecoration.underline,
+              ),),
+            ],
+          ),
+        )
+
+
+        //2, Pestañas (TabBar)
+
+        //3, Contenido de pestañas (TabBarView)
+
+        //4, Carrito (Cart)
+
+        ],
+
+      ),
+      );
   }
 }
